ab2a80ddc7a4a9a7c0de3e3f1bb548ac312b1a1b
Caroline Huzalo <carolhuzalo@gmail.com>
2020-09-26 15:14:51 -0300

    Exercicio 1 - Javascript

---
+++ aulas-javascript/exercicios/aula1/script.js
@@ -0,0 +1,10 @@
+function checarSePodeDirigir() {
+    let idade = prompt('Digite sua idade:');
+    if (idade >= 18){
+        alert('Ja pode dirigir!');
+    }else{
+        alert('Ainda não pode dirigir!');
+    }
+    
+}
+checarSePodeDirigir();
